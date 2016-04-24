c4bc0806358dbe6c17fefa190076eb800829ae8e
narayananembar <indix@Narayanans-MacBook-Air.local>
2016-04-24 21:36:37 +0530

    Content changes on homepage

--- app/js/pages/Index.jsx
+++ app/js/pages/Index.jsx
@@ -94,8 +94,8 @@ var Index = React.createClass({
           <input className="lr-8" type="text" onChange={this.onUpdate} value={this.state.input} ref="input"/>
           <button onClick={this.submit} ref="button"><i className="material-icons">search</i></button>
           <div className="hint">
-            <span id="bold">Search anything. </span>
-            <span>We care for you to get latest information from trusted sources. (Eg: India, Modi, etc)</span>
+            <span id="bold">Anything. </span>
+          <span> Get  latest information from trusted sources.</span>
           </div>
         </div>
         <div className={cn("loader", {none: this.state.visible})}>
